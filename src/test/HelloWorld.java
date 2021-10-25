0beaffaae2d764eaa6b7b28b6169cbd73edb4e74
fgguo <1198316546@qq.com>
2021-10-25 20:25:00 +0800

    init repo

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author fuguo
+ * @date 2021/10/25 7:36 下午
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
