674739950520d15ddbc8ab529a0036541d38ca19
fgguo <1198316546@qq.com>
2021-10-27 20:55:47 +0800

    add object and class.

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -7,10 +7,15 @@ package test;
 public class HelloWorld {
     final String SMALL = "small";
     final String MEDIUM = "medium";
+    String name = "ds";
     enum Size {SMALL,MEDIUM};
+    HelloWorld(String n){
+        String name = n;
+    }
     public static void main(String[] args) {
         Size s = Size.SMALL;
         System.out.println(s);
-
+        HelloWorld helloWorld = new HelloWorld("mu");
+        System.out.println(helloWorld.name);
     }
 }
