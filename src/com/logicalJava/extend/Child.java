0f0d6801ab30b9c705e990266a7caede3a43e785
fgguo <1198316546@qq.com>
2021-11-20 10:31:14 +0800

    新增Java编程的逻辑笔记

---
+++ src/com/logicalJava/extend/Child.java
@@ -0,0 +1,40 @@
+package com.logicalJava.extend;
+
+/**
+ * @className: Child
+ * @description: TODO 类描述
+ * @author: fuguo
+ * @date: 2021/11/19
+ **/
+public class Child extends Base{
+    public static int s;
+    private int a;
+    static {
+        System.out.println("子类静态代码块，s: " + s);
+        s = 10;
+    }
+    {
+        System.out.println("子类实例代码块，a: "+ a);
+        a = 10;
+    }
+    public Child(){
+        System.out.println("子类构造方法，a :" + a);
+        a = 20;
+    }
+    protected void step(){
+        System.out.println("Child s :" + s + " Child a: " + a);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("------new Child()-------");
+        Child c = new Child();
+        System.out.println("-------c.action-----");
+        c.action();
+        Base b = c;
+        System.out.println("-------b.action()--------");
+        b.action();
+        System.out.println("-------b.s: " + b.s);
+        System.out.println("-------c.s: " + c.s);
+    }
+}
